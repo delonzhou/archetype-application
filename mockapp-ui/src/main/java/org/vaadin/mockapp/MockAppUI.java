3505f134d1a0200df7eb2d31d3d46a544002da26
Henri Sara <hesara@vaadin.com>
2014-12-16 15:13:15 +0200

    Convert application-example to application project (#15393)
    Change-Id: Idbf90a9b824bcacfbe6bdaff626e7cf280322cab

--- mockapp-ui/src/main/java/org/vaadin/mockapp/MockAppUI.java
+++ mockapp-ui/src/main/java/org/vaadin/mockapp/MockAppUI.java
@@ -1,86 +1,44 @@
 package org.vaadin.mockapp;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.vaadin.mockapp.samples.MainScreen;
-import org.vaadin.mockapp.samples.authentication.AccessControl;
-import org.vaadin.mockapp.samples.authentication.BasicAccessControl;
-import org.vaadin.mockapp.samples.authentication.LoginScreen;
-import org.vaadin.mockapp.samples.authentication.LoginScreen.LoginListener;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
-import com.vaadin.server.BootstrapFragmentResponse;
-import com.vaadin.server.BootstrapListener;
-import com.vaadin.server.BootstrapPageResponse;
-import com.vaadin.server.Responsive;
-import com.vaadin.server.ServiceException;
-import com.vaadin.server.SessionInitEvent;
-import com.vaadin.server.SessionInitListener;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
 /**
- * 
+ *
  */
 @Theme("mockapp")
 @Widgetset("org.vaadin.mockapp.MockAppWidgetset")
 public class MockAppUI extends UI {
 
-    private AccessControl accessControl = new BasicAccessControl();
-
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        Responsive.makeResponsive(this);
-        setLocale(vaadinRequest.getLocale());
-        getPage().setTitle("MockApp");
-        if (!accessControl.isUserSignedIn()) {
-            setContent(new LoginScreen(accessControl, new LoginListener() {
-                @Override
-                public void loginSuccessful() {
-                    showMainView();
-                }
-            }));
-        } else {
-            showMainView();
-        }
-    }
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setMargin(true);
+        setContent(layout);
 
-    protected void showMainView() {
-        setContent(new MainScreen(MockAppUI.this));
-        getNavigator().navigateTo(getNavigator().getState());
-    }
+        Button button = new Button("Click Me");
+        button.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(ClickEvent event) {
+                layout.addComponent(new Label("Thank you for clicking"));
+            }
+        });
+        layout.addComponent(button);
 
-    public static MockAppUI get() {
-        return (MockAppUI) UI.getCurrent();
-    }
-
-    public AccessControl getAccessControl() {
-        return accessControl;
     }
 
     @WebServlet(urlPatterns = "/*", name = "MockAppUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MockAppUI.class, productionMode = false)
     public static class MockAppUIServlet extends VaadinServlet {
-        @Override
-        protected void servletInitialized() throws ServletException {
-            super.servletInitialized();
-            /*
-             * Configure the viewport meta tags appropriately on mobile devices.
-             * Instead of device based scaling (default), using responsive
-             * layouts.
-             * 
-             * If using Vaadin TouchKit, this is done automatically and it is
-             * sufficient to have an empty servlet class extending
-             * TouchKitServlet.
-             */
-            getService().addSessionInitListener(
-                    new ViewPortSessionInitListener());
-        }
     }
 }
